b593c0315007563c6e34d79242911e7e6d79e5bd
Naccache Robert <NaccacheR@piraeusbank.gr>
2017-12-06 16:35:45 +0200

    Fixed the get request

--- 5. JS - Create User/index.js
+++ 5. JS - Create User/index.js
@@ -1,5 +1,30 @@
-fetch(url)
-.then(handleErrors)
-.then(parseJson)
-.then(updateprofile)
-.catch(printError)
+var btn = document.querySelector("#btn");
+var avatar = document.querySelector("#avatar");
+var fullname = document.querySelector("#fullname");
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var city = document.querySelector("#city");
+
+
+
+var url = "https://randomuser.me/api";
+
+btn.addEventListener("click", function() {
+  fetch(url)
+    .then(function(req){
+      if(!req.ok){
+        throw Error(req.status);
+      }
+      return req;
+    })
+    .then(function(data){
+      return data.json();
+    })
+    .then(function(jsonData){
+      console.log(jsonData.results.email);
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+
+});
