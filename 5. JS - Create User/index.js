3708a5e59104c316714649f0edcdf05ed97142c5
unknown <rnaccache@gmail.com>
2017-12-06 18:11:57 +0200

    finalized exercise

--- 5. JS - Create User/index.js
+++ 5. JS - Create User/index.js
@@ -6,25 +6,47 @@ var email = document.querySelector("#email");
 var city = document.querySelector("#city");
 
 
-
 var url = "https://randomuser.me/api";
 
+//add an even listener
 btn.addEventListener("click", function() {
-  fetch(url)
-    .then(function(req){
-      if(!req.ok){
-        throw Error(req.status);
-      }
-      return req;
-    })
-    .then(function(data){
-      return data.json();
-    })
-    .then(function(jsonData){
-      console.log(jsonData.results.email);
-    })
-    .catch(function(err){
-      console.log(err);
-    })
-
+//Start promise chaining
+fetch(url)
+    .then(errorCheck)
+    .then(parseJson)
+    .then(displayInfo)
+    .catch(error)
 });
+
+
+
+// Functions for Promises
+
+// function to check for errors
+function errorCheck(req) {
+    if(!req.ok){
+        throw Error(req.status);
+    }
+    return req;
+}
+
+// function to parse data to json and return first value
+function parseJson (res){
+    return res.json().then(function(data){
+        return data.results[0];
+    })
+}
+
+// Function to display information to the user
+function displayInfo(person){
+    fullname.innerHTML = person.name.first + person.name.last;
+    username.innerHTML = person.login.username;
+    email.innerHTML = person.email;
+    city.innerHTML = person.location.city;
+    avatar.src = person.picture.medium;
+}
+
+//what to do on error
+function error(err){
+    console.log(err);
+}
